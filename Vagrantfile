2538ed6ca46cc360ac5e9f382a23324673b8e68e
Xiangyu John Wu <john@careerharbour.co.uk>
2016-05-07 09:58:14 +0200

    Optimised heroku-local networking, added Postgres
    Did you use Linter?
    Yes
    
    1. What exactly did you do, explain step by step using an ordered list?
    
    a. Changed the port forwarding from guest to host to 5000 instead of 8000 as the former is the default for heroku local web command.
    b. Added the installation of postgresql and its necessary dependencies in the vagrant provisioning.
    
    2. Why did you do it? Any comments?
    
    Now you can run heroku local web and access the local server running at localhost:5000 in your browser. This separates local testing and deployment but allows you to launch
    local testing directly from within the vagrant ssh and inside the virtualenv.

--- Vagrantfile
+++ Vagrantfile
@@ -7,7 +7,7 @@ Vagrant.configure("2") do |config|
 
     # Create forwarding ports for client-guest machine access via localhost.
     config.vm.network :forwarded_port, guest: 80, host: 8080
-    config.vm.network :forwarded_port, guest: 8000, host: 8000
+    config.vm.network :forwarded_port, guest: 5000, host: 5000
 
     # Provision the virtual machine.
     config.vm.provision :shell, :path => "provision.sh"
--- provision.sh
+++ provision.sh
@@ -7,6 +7,7 @@ echo "---------------------------------------------"
 sudo apt-get update
 sudo apt-get install -y python-pip
 sudo wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+sudo apt-get install -y python-dev libpq-dev postgresql postgresql-contrib
 sudo pip install virtualenvwrapper
 cd /vagrant && virtualenv --python=python3.4 myvenv && source myvenv/bin/activate && pip install -r /vagrant/requirements.txt && django-admin startproject myproject
 
