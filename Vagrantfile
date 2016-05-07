89a440890c8d938510b54b47760c9f16bbf59d6b
alayek <arijit.layek1991@gmail.com>
2016-05-07 16:31:51 +0530

    update vagrantfile and provisioner to work with virtualenvwrapper

--- Vagrantfile
+++ Vagrantfile
@@ -8,6 +8,14 @@ Vagrant.configure("2") do |config|
     # Create forwarding ports for client-guest machine access via localhost.
     config.vm.network :forwarded_port, guest: 80, host: 8080
     config.vm.network :forwarded_port, guest: 8000, host: 8000
+    
+    # Add the tty fix as mentioned in issue 1673 on vagrant repo
+    # To avoid 'stdin is not a tty' messages
+    # vagrant provisioning in shell runs bash -l
+    config.vm.provision "fix-no-tty", type: "shell" do |s|
+        s.privileged = false
+        s.inline = "sudo sed -i '/tty/!s/mesg n/tty -s \\&\\& mesg n/' /root/.profile"
+    end
 
     # Provision the virtual machine.
     config.vm.provision :shell, :path => "provision.sh"
--- provision.sh
+++ provision.sh
@@ -1,15 +1,53 @@
 #!/usr/bin/env bash
 
+# installation settings
+PROJECT="my_project" # we would want a name passed to it via te first argument, $1
+DB="fcc_provision" # the name of postgreSQL DB we need to provision, maybe $2
+ENV_NAME="fcc-py3" # the vitualenv we would like to create, with Python 3.4
+
+# This file is executed by root user - sudo not needed
+# But do not create any directory 
+# which vagrant user might need access to later in su mode 
+# use su - vagrant -c "" syntax
+
 echo "---------------------------------------------"
 echo "Running vagrant provisioning"
 echo "---------------------------------------------"
 
-sudo apt-get update
-sudo apt-get install -y python-pip
-sudo wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-sudo pip install virtualenvwrapper
-cd /vagrant && virtualenv --python=python3.4 myvenv && source myvenv/bin/activate && pip install -r /vagrant/requirements.txt && django-admin startproject myproject
+echo "------------- Updating packages and their dependencies -----------"
+apt-get update -y # no need for sudo, and -y is needed to bypass the yes-no
+echo "-------------- Installing build-essential and pip ----------------"
+apt-get install -y build-essential python-pip
+
+# install heroku toolbelt
+echo "-------------- Installing heroku toolbelt -------------------------"
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+# install virtualenv and virtualenvwrapper
+echo "--------------- Installing both virtualenv and virtualenvwrapper ---"
+pip install virtualenvwrapper virtualenv
+
+echo "--------------- Preparing .bashrc for first usage ------------------"
+# set up virtualenvwrapper
+echo WORKON_HOME="/home/vagrant/.virtualenvs" >> /home/vagrant/.bashrc
+# activate it
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> /home/vagrant/.bashrc
+echo "WORKON_HOME=\"/home/vagrant/.virtualenvs\"" >> /home/vagrant/.bashrc
+
+echo "---------------- Creating virtual environment with Python 3 ---------"
+# Remember to run in vagrantt user mode
+# Otherwise, the user would not be able to run pip install without sudo
+su - vagrant -c "/usr/local/bin/virtualenv /home/vagrant/.virtualenvs/${ENV_NAME} --python=/usr/bin/python3 && \
+    /home/vagrant/.virtualenvs/${ENV_NAME}/bin/pip install -r /vagrant/requirements.txt"
+
+#cd /vagrant && virtualenv --python=python3.4 myvenv && source myvenv/bin/activate && pip install -r /vagrant/requirements.txt && django-admin startproject myproject
+
+# take the user to shared directory
+echo "cd /vagrant" >> /home/vagrant/.bashrc
+
+# Activate the virtualenv on first login
+echo "workon ${ENV_NAME}" >> /home/vagrant/.bashrc
 
 echo "---------------------------------------------"
-echo " Finished."
+echo " Done! Run vagrant ssh to start working "
 echo "---------------------------------------------"
