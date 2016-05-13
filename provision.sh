7beffc03223858bc5df62872752a056ac6615cd0
alayek <arijit.layek1991@gmail.com>
2016-05-14 00:35:56 +0530

    Fix some red colored text in provisioning

--- provision.sh
+++ provision.sh
@@ -9,7 +9,7 @@ ENV_NAME="fcc-py3" # the vitualenv we would like to create, with Python 3.4
 # But do not create any directory
 # which vagrant user might need access to later in su mode
 # use su - vagrant -c "" syntax
-
+export DEBIAN_FRONTEND=noninteractive
 echo "---------------------------------------------"
 echo "Running vagrant provisioning"
 echo "---------------------------------------------"
@@ -22,43 +22,50 @@ echo "-------------- Installing heroku toolbelt -------------------------"
 # add heroku repository to apt
 echo "deb http://toolbelt.heroku.com/ubuntu ./" > /etc/apt/sources.list.d/heroku.list
 # install heroku's release key for package verification
-wget -O- https://toolbelt.heroku.com/apt/release.key | apt-key add -
+wget -O- https://toolbelt.heroku.com/apt/release.key 2>&1 | apt-key add -
 
-echo "------------- Updating packages and their dependencies -----------"
+echo "---------------------------------------------"
+echo "------- Updating package dependencies -------"
+echo "---------------------------------------------"
 apt-get update -y # no need for sudo, and -y is needed to bypass the yes-no
 
-# install the toolbelt
-apt-get install heroku-toolbelt --no-install-recommends -y
-
-echo "-------------- Installing build-essential and pip ----------------"
+echo "---------------------------------------------"
+echo "-------- Installing packages ----------------"
+echo "---------------------------------------------"
 # install gcc and g++ and other build basics to ensure most software works
 # install man too
 # dos2unix is needed because we could have CR-LF line terminator on Windows
 # And that would prevent ~/.bashrc to work properly because \r would be unrecognized
-apt-get install build-essential dos2unix python-pip man -y --no-install-recommends
-
-# needed for heroku toolbelt
+# Ruby needed for heroku toolbelt
 # notice that this is not a rigorous Ruby install, where we typically use rvm
-apt-get install ruby --no-install-recommends -y
+apt-get install -y --no-install-recommends heroku-toolbelt build-essential dos2unix python-pip man ruby python-dev python3-dev libpq-dev postgresql postgresql-contrib 
+# install the cli
+su - vagrant -c "heroku --version > /dev/null 2>&1"
 
 # install virtualenv and virtualenvwrapper
-echo "--------------- Installing both virtualenv and virtualenvwrapper ---"
-pip install virtualenvwrapper virtualenv
+echo "---------------------------------------------"
+echo "------- Installing virtualenvwrapper --------"
+echo "---------------------------------------------"
+pip install virtualenvwrapper
 
 # install postgresql and setup user
-echo "--------------- Installing postgresql ------------------------------"
-apt-get install python-dev python3-dev libpq-dev postgresql postgresql-contrib --no-install-recommends -y
+echo "---------------------------------------------"
+echo "------- Setting up postgresql ---------------"
+echo "---------------------------------------------"
 su - postgres -c "createuser -s vagrant"
 su - vagrant -c "createdb ${DB}"
 
-echo "--------------- Preparing .bashrc for first usage ------------------"
-# set up virtualenvwrapper
-echo WORKON_HOME="/home/vagrant/.virtualenvs" >> /home/vagrant/.bashrc
+echo "---------------------------------------------"
+echo "------ Preparing .bashrc for first usage ----"
+echo "---------------------------------------------"
+# set up virtualenvwrapper and add code to properly
 # activate virtualenvwrapper
 echo "WORKON_HOME=\"/home/vagrant/.virtualenvs\"" >> /home/vagrant/.bashrc
 echo "source /usr/local/bin/virtualenvwrapper.sh > /dev/null 2>&1" >> /home/vagrant/.bashrc
 
-echo "---------------- Creating virtual environment with Python 3 ---------"
+echo "---------------------------------------------"
+echo "------ Creating Python 3 environment --------"
+echo "---------------------------------------------"
 # Remember to run in vagrantt user mode
 # Otherwise, the user would not be able to run pip install without sudo
 su - vagrant -c "/usr/local/bin/virtualenv /home/vagrant/.virtualenvs/${ENV_NAME} --python=/usr/bin/python3 && \
