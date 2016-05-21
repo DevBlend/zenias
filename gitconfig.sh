7bade481fa5c57534b4f69f6f80cacf8acea0908
John Wu <john@careerharbour.co.uk>
2016-05-21 19:21:48 +0200

    Minor changes towards two files

--- gitconfig.sh
+++ gitconfig.sh
@@ -6,9 +6,6 @@ gitConfig () {
     git config --global push.default simple
 }
 
-if [ ! -f ~/.gitconfig ]
-then
-    read -p "What is your real name? " username
-    read -p "What is your Github email address? " email
-    gitConfig
-fi
+read -p "What is your real name? " username
+read -p "What is your Github email address? " email
+gitConfig
