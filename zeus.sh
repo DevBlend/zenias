59c9604df6022aa7dcb9fca6d78853781e9ac5e5
John Wu <john@careerharbour.co.uk>
2016-05-23 02:55:59 -0700

    Add new command for gitCreate to create github remote repo

--- zeus.sh
+++ zeus.sh
@@ -1,16 +1,36 @@
 #!/usr/bin/env bash
 
 gitConfig () {
-    read -p "What is your real name? " username
-    read -p "What is your Github email address? " email
+    read -p "What is your real name? >>> " username
+    read -p "What is your Github email address? >>> " email
+    read -p "what is your Github username? >>> " githubusername
     git config --global user.email "$email"
     git config --global user.name "$username"
     git config --global push.default simple
+    export githubname=$githubusername
+}
+
+gitCreate () {
+    read -p "How should the Github remote repo be called? >>> " remoterepo
+    case $2 in
+        private)
+            curl -u "$githubname" https://api.github.com/user/repos -d '{"name":'"\"$remoterepo\""',"private":"true"}'
+            ;;
+        public)
+            curl -u "$githubname" https://api.github.com/user/repos -d '{"name":'"\"$remoterepo\""'}'
+            ;;
+        *)
+            echo "Invalid argument parsed"
+            ;;
+    esac
 }
 
 case $1 in
     gitConfig)
         gitConfig
+        ;;
+    gitCreate)
+        gitCreate
         ;;
     *)
         echo "Invalid argument parsed"
