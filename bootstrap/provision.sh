0de09177ad5869513b00cf1452f60f1be102305f
John Wu <xiangyu.wu.10@gmail.com>
2016-05-30 19:51:31 +0200

    Add full support for static bootstrap projects
    - Launching zeus create -bootstrap newproject
      will create a new director called newproject
      in which there is everything installed
      for you to push to heroku and deploy a
      template static website with bootstrap.
      You just need to add your customisations.
    - Changed cp to mv for making zeus and .bashrc
      disappear for -python and -bootstrap,
      because users really should not have these
      in their /vagrant synced folder due to
      distractions. They are junk code not needed for
      the actual user.
    - bootstrap directory with all needed files added
      for proper heroku deployment
    - zeus upgraded to accommodate the new -bootstrap option.

--- bootstrap/provision.sh
+++ bootstrap/provision.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# installation settings
-PROJECT="my_project" # we would want a name passed to it via te first argument, $1
-DB="fcc_provision" # the name of postgreSQL DB we need to provision, maybe $2
-ENV_NAME="fcc-py3" # the vitualenv we would like to create, with Python 3.4
-
 # This file is executed by root user - sudo not needed
 # But do not create any directory
 # which vagrant user might need access to later in su mode
@@ -38,38 +33,13 @@ echo "---------------------------------------------"
 # And that would prevent ~/.bashrc to work properly because \r would be unrecognized
 # Ruby needed for heroku toolbelt
 # notice that this is not a rigorous Ruby install, where we typically use rvm
-apt-get install -y --no-install-recommends heroku-toolbelt build-essential dos2unix python-pip man ruby python3-dev libpq-dev postgresql postgresql-contrib curl
+apt-get install -y --no-install-recommends heroku-toolbelt build-essential dos2unix man curl
 # install the cli
 su - vagrant -c "heroku --version > /dev/null 2>&1"
-
-# install virtualenv and virtualenvwrapper
-echo "---------------------------------------------"
-echo "------- Installing virtualenvwrapper --------"
-echo "---------------------------------------------"
-pip install virtualenvwrapper
-
-# install postgresql and setup user
-echo "---------------------------------------------"
-echo "------- Setting up postgresql ---------------"
-echo "---------------------------------------------"
-su - postgres -c "createuser -s vagrant"
-su - vagrant -c "createdb ${DB}"
 
 echo "---------------------------------------------"
 echo "------ Preparing .bashrc for first usage ----"
 echo "---------------------------------------------"
-# set up virtualenvwrapper and add code to properly
-# activate virtualenvwrapper
-echo "WORKON_HOME=\"/home/vagrant/.virtualenvs\"" >> /home/vagrant/.bashrc
-echo "source /usr/local/bin/virtualenvwrapper.sh > /dev/null 2>&1" >> /home/vagrant/.bashrc
-
-echo "---------------------------------------------"
-echo "------ Creating Python 3 environment --------"
-echo "---------------------------------------------"
-# Remember to run in vagrantt user mode
-# Otherwise, the user would not be able to run pip install without sudo
-su - vagrant -c "/usr/local/bin/virtualenv /home/vagrant/.virtualenvs/${ENV_NAME} --python=/usr/bin/python3 && \
-    /home/vagrant/.virtualenvs/${ENV_NAME}/bin/pip install -r /vagrant/requirements.txt"
 
 su - vagrant -c "mv /vagrant/.bashrc /home/vagrant/"
 su - vagrant -c "mkdir /home/vagrant/.configs"
@@ -77,9 +47,6 @@ su - vagrant -c "mv /vagrant/zeus /home/vagrant/.configs/zeus"
 # If you are on Windows host, with Git checkout windows line terminator style CRLF
 # this comes in handy
 su - vagrant -c "dos2unix  /home/vagrant/.bashrc > /dev/null 2>&1"
-
-# Activate the virtualenv on first login
-echo "workon ${ENV_NAME}" >> /home/vagrant/.bashrc
 
 echo "---------------------------------------------"
 echo " Done! Run vagrant ssh to start working "
