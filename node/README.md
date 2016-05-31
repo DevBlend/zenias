62435b2da1f41764cc0b59333b5618b124dad6d4
Koustuv Sinha <koustuvsinha@hotmail.com>
2016-05-31 13:11:18 +0530

    Add Node vagrant files to Zeus

--- node/README.md
+++ node/README.md
@@ -1,5 +1,56 @@
-# Directory for node setup
+# NodeSchool-Vagrant
 
-This directory contains all the node related setup for zeus.
+Vagrant Box to have all [Nodeschool](http://nodeschool.io) workshoppers as a dependency. Primarily made for [Nodeschool IEM-Kolkata International Day Workshop](http://nodeschool.io/iem-kolkata/).
 
-For collaborators who are specialised in node, please change files only here.
+Motivated by [ByteKnacker](https://github.com/byteknacker)'s awesome [fcc-python-vagrant](https://github.com/byteknacker/fcc-python-vagrant) Project and now merged with [Zeus](https://github.com/alayek/zeus) Project.
+
+## Features
+
+All dependencies are listed in `provision.sh` to be installed.
+
+* Node version : 4.4.3 (Installed via nvm)
+* Npm version : 2.15.1
+
+Workshopper modules installed :
+
+* javascripting
+* learnyounode
+* git-it
+* how-to-npm
+* scope-chains-closures
+* stream-adventure
+* elementary-electron
+* how-to-markdown
+* functional-javascript-workshop
+* expressworks
+* promise-it-wont-hurt
+* count-to-6
+
+Other programs installed :
+
+* Heroku toolbelt
+* Heroku CLI
+
+## Installation
+
+To run this vagrant box, you must have the following dependencies installed :
+
+- [Virtualbox](https://www.virtualbox.org/wiki/Downloads) latest (currently version 5.0.20)
+- [VirtualBox 5.0.20 Oracle VM VirtualBox Extension Pack](http://download.virtualbox.org/virtualbox/5.0.20/Oracle_VM_VirtualBox_Extension_Pack-5.0.20-106931.vbox-extpack), compatible with latest Virtualbox
+- [Vagrant](https://www.vagrantup.com/downloads.html), (currently version 1.8.1)
+- [Git Bash](https://git-scm.com/downloads) (only if you are on Windows)
+
+If you are on Windows, restart your local machine after the installation of these programmes.
+
+### First Time Usage
+
+1. Run `git clone https://github.com/alayek/zeus.git`
+2. Inside the directory path/to/node/ run `vagrant up` in your bash-compliant terminal (Git Bash on Windows, regular terminal on Linux or Mac).
+3. Inside the directory path/to/node/ run `vagrant ssh` to start the session inside the development VM.
+4. Start coding and forget about development environment setups.
+
+Note that you end up inside the Vagrant VM after these four steps. The VM is isolated from your local machine. Your current working directory has the absolute path of `/vagrant`, which is termed synced folder. It is in real-time sync with your `path/to/nodeschool-vagrant/` local git repo. That is how your local machine communicates with the Vagrant VM.
+
+## LICENSE
+
+[MIT](https://koustuvs.mit-license.org/)
---
+++ node/provision.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+
+# installation settings
+PROJECT="nodeschool" # we would want a name passed to it via te first argument, $1
+DB="nodeschool" # the name of postgreSQL DB we need to provision, maybe $2
+
+# This file is executed by root user - sudo not needed
+# But do not create any directory
+# which vagrant user might need access to later in su mode
+# use su - vagrant -c "" syntax
+export DEBIAN_FRONTEND=noninteractive
+echo "---------------------------------------------"
+echo "Running vagrant provisioning"
+echo "---------------------------------------------"
+
+# install heroku toolbelt
+echo "-------------- Installing heroku toolbelt -------------------------"
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+# These shell script snippets are directly taken from heroku installation script
+# We want to avoid the apt-get update
+# add heroku repository to apt
+echo "deb http://toolbelt.heroku.com/ubuntu ./" > /etc/apt/sources.list.d/heroku.list
+# install heroku's release key for package verification
+wget -O- https://toolbelt.heroku.com/apt/release.key 2>&1 | apt-key add -
+
+echo "---------------------------------------------"
+echo "------- Updating package dependencies -------"
+echo "---------------------------------------------"
+apt-get update -y # no need for sudo, and -y is needed to bypass the yes-no
+
+echo "---------------------------------------------"
+echo "-------- Installing packages ----------------"
+echo "---------------------------------------------"
+# install gcc and g++ and other build basics to ensure most software works
+apt-get install -y --no-install-recommends heroku-toolbelt build-essential
+# install the cli
+su - vagrant -c "heroku --version > /dev/null 2>&1"
+
+echo "---------------------------------------------"
+echo "-------- Installing NVM & Node --------------"
+echo "---------------------------------------------"
+
+# install nvm
+su - vagrant -c "wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash"
+
+# source bash and install node
+su - vagrant <<NVM
+
+source ~/.nvm/nvm.sh
+echo 'sourcing done'
+nvm install 4.4.3
+
+NVM
+
+echo "---------------------------------------------"
+echo "--- Installing Workshopper modules ----------"
+echo "---------------------------------------------"
+
+su - vagrant <<WORKSHOPPERS
+
+source ~/.nvm/nvm.sh
+echo "Installing javascripting ..."
+npm i -g javascripting
+echo "Installing learnyounode ..."
+npm i -g learnyounode
+echo "Installing git-it ..."
+npm i -g git-it
+echo "Installing how-to-npm ..."
+npm i -g how-to-npm
+echo "Installing scope-chains-closures ..."
+npm i -g scope-chains-closures
+echo "Installing stream-adventure ..."
+npm i -g stream-adventure
+echo "Installing elementary-electron ..."
+npm i -g elementary-electron
+echo "Installing how-to-markdown ..."
+npm i -g how-to-markdown
+echo "Installing functional-javascript-workshop ..."
+npm i -g functional-javascript-workshop
+echo "Installing expressworks ..."
+npm i -g expressworks
+echo "Installing promise-it-wont-hurt ..."
+npm i -g promise-it-wont-hurt
+echo "Installing count-to-6 ..."
+npm i -g count-to-6
+
+echo "All workshoppers installed."
+
+WORKSHOPPERS
+
+echo "---------------------------------------------"
+echo " Done! Run vagrant ssh to start working "
+echo "---------------------------------------------"
