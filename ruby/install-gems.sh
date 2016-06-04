1e68495aa91ede0b03e35736123d81c33f304274
Zac Cassini <zcassini@gmail.com>
2016-06-03 22:34:33 -0400

    remove 1 chruby error, comment, clean up and rename provision2

---
+++ ruby/install-gems.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# where we're going we don't need docs
+echo 'gem: --no-ri --no-rdoc' >> ~/.gemrc
+
+echo "---------------------------------------------"
+echo " ------------ Installing Gems--------------- "
+echo "---------------------------------------------"
+# install a couple of common gems
+echo "------------bundler--------------------------"
+gem install bundler -q -v '~> 1.12'
+echo "------------rails----------------------------"
+gem install rails -q -v '~>4.2'
+echo "------------rspec----------------------------"
+gem install rspec -q -v '~> 3.4'
+echo "------------sinatra--------------------------"
+gem install sinatra -q -v '~> 1.4'
+
+# restore our original .bashrc
+mv ~/.bashrc.bak ~/.bashrc
+
+echo "---------------------------------------------"
+echo " Done! Run vagrant ssh to start working      "
+echo "---------------------------------------------"
+
+exit
